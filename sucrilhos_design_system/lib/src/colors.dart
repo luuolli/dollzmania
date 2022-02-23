4ff024a93e38d69eca7b3602a7095ccd6d8d3420
Lucas Pereira de Oliveira <lucas.pereira.oli.dev@hotmail.com>
2022-02-22 23:36:40 -0300

    updated color

--- sucrilhos_design_system/lib/src/colors.dart
+++ sucrilhos_design_system/lib/src/colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static Color get white => const Color(0xFFFFFFFF);
   static Color get darkBlue => const Color(0xFF0D5878);
-  static Color get lightPink => const Color(0xFFF0E8FF);
+  static Color get lightPink => const Color(0xFFFAF2FF);
   static Color get purple => const Color(0xFF8552D9);
   static Color get black => const Color(0xFF141414);
   static Color get black10 => const Color(0xFF505050);
