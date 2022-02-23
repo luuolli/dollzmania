9d80aed4fd1d99ccf5219924d56736de39bf27af
Lucas Pereira de Oliveira <lucas.pereira.oli.dev@hotmail.com>
2022-02-22 21:51:09 -0300

    initialized themes

--- sucrilhos_design_system/lib/src/themes/dark_theme.dart
+++ sucrilhos_design_system/lib/src/themes/dark_theme.dart
@@ -1,1 +1,6 @@
-// TODO Implement this library.
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+final ThemeData darkTheme = ThemeData.dark().copyWith();
+
+final CupertinoThemeData cupertinoDarkTheme = CupertinoThemeData().copyWith();
--- sucrilhos_design_system/lib/src/themes/light_theme.dart
+++ sucrilhos_design_system/lib/src/themes/light_theme.dart
@@ -1,1 +1,6 @@
-// TODO Implement this library.
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+final ThemeData lightTheme = ThemeData.dark().copyWith();
+
+final CupertinoThemeData cupertinoLightTheme = CupertinoThemeData().copyWith();
