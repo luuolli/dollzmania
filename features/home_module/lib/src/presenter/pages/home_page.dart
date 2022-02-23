bc7ea593216738d917fc4356087b303e3b2bf59f
Lucas Pereira de Oliveira <lucas.pereira.oli.dev@hotmail.com>
2022-02-22 23:37:52 -0300

    using theme from context

--- features/home_module/lib/src/presenter/pages/home_page.dart
+++ features/home_module/lib/src/presenter/pages/home_page.dart
@@ -29,32 +29,25 @@ class HomePage extends StatelessWidget {
                     children: [
                       TextSpan(
                         text: 'Bruna Edition',
-                        style: theme.textTheme.caption?.copyWith(
-                          color: Colors.white.withOpacity(0.6),
-                        ),
+                        style: theme.textTheme.caption,
                       ),
                     ],
                   ),
+                  softWrap: true,
+                  overflow: TextOverflow.clip,
                   textAlign: TextAlign.end,
-                  style: theme.textTheme.headline4?.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
+                  style: theme.textTheme.headline1,
                 ),
               ],
             ),
             CupertinoTextField(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 12,
-                vertical: 10,
-              ),
+              padding: theme.buttonTheme.padding,
               decoration: BoxDecoration(
-                color: const Color(0x50F0E8FF),
+                color: theme.colorScheme.onBackground.withOpacity(0.3),
                 borderRadius: BorderRadius.circular(4),
               ),
               placeholder: 'Nome',
               placeholderStyle: theme.textTheme.bodyText2?.copyWith(
-                color: Colors.white.withOpacity(0.9),
                 height: 1.4,
               ),
             ),
@@ -65,14 +58,14 @@ class HomePage extends StatelessWidget {
                   borderRadius: BorderRadius.circular(8),
                   boxShadow: [
                     BoxShadow(
-                      color: const Color(0xFF003A5A).withOpacity(0.5),
+                      color: theme.primaryColor.withOpacity(0.5),
                       offset: const Offset(2, 3),
                       spreadRadius: 1.5,
                     ),
                   ],
                 ),
                 child: CupertinoButton(
-                  color: const Color(0xFF0D5878),
+                  color: theme.primaryColor,
                   child: const Text('Jogar'),
                   onPressed: () {},
                 ),
