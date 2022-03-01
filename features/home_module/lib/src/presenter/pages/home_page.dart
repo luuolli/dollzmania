ecd8f269f4f2b1a1c254853735d148180c656e02
Lucas Pereira de Oliveira <lucas.pereira.oli.dev@hotmail.com>
2022-02-28 23:47:40 -0300

    - added dependency home module at app - updated page

--- features/home_module/lib/src/presenter/pages/home_page.dart
+++ features/home_module/lib/src/presenter/pages/home_page.dart
@@ -6,66 +6,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final mediaQuery = MediaQuery.of(context);
-
-    return SafeArea(
-      minimum: const EdgeInsets.symmetric(horizontal: 30),
-      child: CupertinoPageScaffold(
-        backgroundColor: Colors.transparent,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset(
-                  'assets/images/girl.png',
-                  height: mediaQuery.size.width * 0.5,
-                ),
-                Text.rich(
-                  TextSpan(
-                    text: 'Dollzmania\n',
-                    children: [
-                      TextSpan(
-                        text: 'Bruna Edition',
-                        style: theme.textTheme.caption,
-                      ),
-                    ],
-                  ),
-                  softWrap: true,
-                  overflow: TextOverflow.clip,
-                  textAlign: TextAlign.end,
-                  style: theme.textTheme.headline1,
-                ),
-              ],
-            ),
-            CupertinoTextField(
-              padding: theme.buttonTheme.padding,
-              decoration: BoxDecoration(
-                color: theme.colorScheme.onBackground.withOpacity(0.3),
-                borderRadius: BorderRadius.circular(4),
-              ),
-              placeholder: 'Nome',
-              placeholderStyle: theme.textTheme.bodyText2?.copyWith(
-                height: 1.4,
-              ),
-            ),
-            SizedBox(
-              width: double.maxFinite,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  boxShadow: AppShadows.defaultShadow(theme),
-                ),
-                child: CupertinoButton(
-                  borderRadius: BorderRadius.circular(8),
-                  color: theme.primaryColor,
-                  child: const Text('Jogar'),
-                  onPressed: () {},
-                ),
-              ),
-            )
-          ],
+    return CupertinoPageScaffold(
+      backgroundColor: AppColors.transparent,
+      child: Center(
+        child: Text(
+          'Oi',
+          style: theme.textTheme.bodyText1,
         ),
       ),
     );
