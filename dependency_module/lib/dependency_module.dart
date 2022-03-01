aa9d8f62ea27e0098490cbbbcc8aac6d5636d188
Lucas Pereira de Oliveira <lucas.pereira.oli.dev@hotmail.com>
2022-02-28 22:59:15 -0300

    updated dependency_module

--- dependency_module/lib/dependency_module.dart
+++ dependency_module/lib/dependency_module.dart
@@ -1,7 +1,3 @@
 library dependency_module;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:flutter_modular/flutter_modular.dart';
